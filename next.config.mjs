b2747c56b52276c914b4fdb4f4c8051c02077cf7
ifrah <ifrah.imtiaz2612@gmail.com>
2026-01-17 03:46:55 +0400

    Fix: Next.js 16 Prisma engine tracing

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  // This fix is required for Next.js 16 + Prisma on Vercel
+  outputFileTracingIncludes: {
+    '/*': ['./node_modules/.prisma/client/**/*'],
+  },
 };
 
 export default nextConfig;
